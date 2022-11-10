cb69bb35e0ed2a4aa7ddbafb48b1eeed82542a94
Nathan <nateyl.b@gmail.com>
2022-11-10 12:33:52 -0800

    changed += to =

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -11,7 +11,7 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
    for(int i = 0; i < n; i ++){
       for(int j = 0; j< n; j++){
-         y[i] += y[i] + A[j + i * n] * x[j];
+         y[i] = y[i] + A[j + i * n] * x[j];
       }
    }
 }
