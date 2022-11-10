37aa40a2c0b0f9f0acc6109bc036d9348d079f0b
Nathan <nateyl.b@gmail.com>
2022-11-10 12:27:18 -0800

    timer added and basic done

--- dgemv-basic.cpp
+++ dgemv-basic.cpp
@@ -1,3 +1,4 @@
+
 const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
 
 /*
@@ -8,4 +9,9 @@ const char* dgemv_desc = "Basic implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of basic matrix multiply
+   for(int i = 0; i < n; i ++){
+      for(int j = 0; j< n; j++){
+         y[i] += y[i] + A[j + i * n] * x[j];
+      }
+   }
 }
