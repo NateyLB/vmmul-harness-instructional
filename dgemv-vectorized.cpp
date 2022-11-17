ed455209ed44b4a13d2ede3fe6248e2f673a3a6b
Nathan <nateyl.b@gmail.com>
2022-11-17 12:21:33 -0800

    changed vectorized to nbasic

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -9,15 +9,11 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
    for(int i = 0; i < n; i ++){
-      for(int j = 0; j< n; j += 8){
+      for(int j = 0; j< n; j ++){
          y[i] = y[i] + A[j + (i * n)] * x[j];
-         y[i] = y[i] + A[j + (i * n) + 1] * x[j+1];
-         y[i] = y[i] + A[j + (i * n) + 2] * x[j+2];
-         y[i] = y[i] + A[j + (i * n) + 3] * x[j+3];
-         y[i] = y[i] + A[j + (i * n) + 4] * x[j+4];
-         y[i] = y[i] + A[j + (i * n) + 5] * x[j+5];
-         y[i] = y[i] + A[j + (i * n) + 6] * x[j+6];
-         y[i] = y[i] + A[j + (i * n) + 7] * x[j+7];
+         // y[i] = y[i] + A[j + (i * n) + 1] * x[j+1];
+         // y[i] = y[i] + A[j + (i * n) + 2] * x[j+2];
+         // y[i] = y[i] + A[j + (i * n) + 3] * x[j+3];
       }
    }
 
