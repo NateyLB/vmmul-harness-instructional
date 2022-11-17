811c3c92be3c4972ca510c95c69dd6520cf8e634
Nathan <nateyl.b@gmail.com>
2022-11-17 11:26:28 -0800

    added n/4 to the j loop

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,7 +8,7 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
-   for(int i = 0; i < (n/4); i ++){
+   for(int i = 0; i < n; i ++){
       for(int j = 0; j< (n/4); j+= 4){
          y[i] = y[i] + A[j + (i * n)] * x[j];
          y[i] = y[i] + A[j + (i * n) + 1] * x[j+1];
