a3306570a62f74410d307c7d5d46d35366da0dd7
Nathan <nateyl.b@gmail.com>
2022-11-17 11:38:39 -0800

    revert

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,13 +8,12 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
-   #pragma clang loop vectorize(assume_safety)
    for(int i = 0; i < n; i ++){
-      for(int j = 0; j< n; j++){
+      for(int j = 0; j< n; j += 4){
          y[i] = y[i] + A[j + (i * n)] * x[j];
-         // y[i] = y[i] + A[j + (i * n) + 1] * x[j+1];
-         // y[i] = y[i] + A[j + (i * n) + 2] * x[j+2];
-         // y[i] = y[i] + A[j + (i * n) + 3] * x[j+3];
+         y[i] = y[i] + A[j + (i * n) + 1] * x[j+1];
+         y[i] = y[i] + A[j + (i * n) + 2] * x[j+2];
+         y[i] = y[i] + A[j + (i * n) + 3] * x[j+3];
       }
    }
 
